fa6e8780f29ebdd3bc421b7f5d63e129c455a422
alextanzilli <alextanzilli@gmail.com>
2016-08-10 18:45:45 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,7 @@ function respond() {
       botRegexPreach = /^\/preach/; botRegexVote = /^\/vote/; botRegexAnyway = /^\/anyway/; botRegexFif = /^\/fif/; botRegexGood = /^\/goodell/;
       botRegexRoar = /^\/roar/; botRegexRespek = /^\/respek/; botRegexFM = /^\/fuckmoney/; botRegexBob = /^\/bobsanders/; botRegexGolf = /^\/golf/;
       botRegexRock = /^\/rockthevote/; botRegexStats = /^\/stats/; botRegexEric = /^\/eric/; botRegexBrain = /^\/brain/; botRegexReject = /^\/rejected/;
+      botRegexSch = /^\/sch/;
       botRegexSiege = /^\/siege/
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -304,7 +305,12 @@ function respond() {
     this.res.writeHead(200);
     postMessage("https://cmgajcpolitics.files.wordpress.com/2014/04/mutombo.jpg")
     this.res.end();
-    }       
+    }   
+    else if(request.text && botRegexSch.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://s-media-cache-ak0.pinimg.com/736x/1d/8a/cd/1d8acd8c6e8e337ce31bef84a8636491.jpg")
+    this.res.end();
+    }   
   else if(request.text && botRegexSiege.test(request.text)) {
     this.res.writeHead(200);
     if(0.6 >= Math.random() > 0.3)
