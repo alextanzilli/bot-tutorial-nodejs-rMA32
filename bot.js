16e4789219f27913608d5d1101f697e9fde9207f
alextanzilli <alextanzilli@gmail.com>
2016-08-28 15:44:26 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ function respond() {
       botRegexPreach = /^\/preach/; botRegexVote = /^\/vote/; botRegexAnyway = /^\/anyway/; botRegexFif = /^\/fif/; botRegexGood = /^\/goodell/;
       botRegexRoar = /^\/roar/; botRegexRespek = /^\/respek/; botRegexFM = /^\/fuckmoney/; botRegexBob = /^\/bobsanders/; botRegexGolf = /^\/golf/;
       botRegexRock = /^\/rockthevote/; botRegexStats = /^\/stats/; botRegexEric = /^\/eric/; botRegexBrain = /^\/brain/; botRegexReject = /^\/rejected/;
-      botRegexSch = /^\/sch/; botRegexGreat = /^\/greatness/;
+      botRegexSch = /^\/sch/; botRegexGreat = /^\/greatness/; botRegexGoat2 = /^\/goat2/;
       botRegexSiege = /^\/siege/
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -316,6 +316,11 @@ function respond() {
     postMessage("http://i.imgur.com/1GKKJi0.jpg")
     this.res.end();
     }       
+      else if(request.text && botRegexGoat2.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://media0.giphy.com/media/pnMm35fFjw16U/giphy.gif")
+    this.res.end();
+    }         
   else if(request.text && botRegexSiege.test(request.text)) {
     this.res.writeHead(200);
     if(0.6 >= Math.random() > 0.3)
