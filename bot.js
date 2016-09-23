5361d4c290d7efcd4f41f7d46001527fddc12844
alextanzilli <alextanzilli@gmail.com>
2016-09-22 22:12:56 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -119,7 +119,7 @@ function respond() {
   }
   else if(request.text && botRegexProp.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://goo.gl/forms/sZDBso2u2HPSRI1r1");
+    postMessage("https://media-mediatemple.netdna-ssl.com/wp-content/uploads/2014/05/00_closed-large.png");
     this.res.end();
   }
     else if(request.text && botRegexRec.test(request.text)) {
