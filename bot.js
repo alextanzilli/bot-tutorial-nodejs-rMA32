632dcf84d51ceb65eec832688220ab5aa86c0215
alextanzilli <alextanzilli@gmail.com>
2016-11-21 10:51:50 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -119,7 +119,7 @@ function respond() {
   }
   else if(request.text && botRegexProp.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://www.clipartkid.com/images/526/sorry-we-are-closed-vinyl-wall-sticker-in-red-color-MZfKY4-clipart.jpg");
+    postMessage("https://goo.gl/forms/Ufc94BnPR9GRPBgs2");
     this.res.end();
   }
     else if(request.text && botRegexRec.test(request.text)) {
