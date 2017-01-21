f0c3f28c38949edb7bf6e2975ba53653af3a87b7
alextanzilli <alextanzilli@gmail.com>
2017-01-21 11:12:34 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,8 @@ function respond() {
       botRegexPreach = /^\/preach/; botRegexVote = /^\/vote/; botRegexAnyway = /^\/anyway/; botRegexFif = /^\/fif/; botRegexGood = /^\/goodell/;
       botRegexRoar = /^\/roar/; botRegexRespek = /^\/respek/; botRegexFM = /^\/fuckmoney/; botRegexBob = /^\/bobsanders/; botRegexGolf = /^\/golf/;
       botRegexRock = /^\/rockthevote/; botRegexStats = /^\/stats/; botRegexEric = /^\/eric/; botRegexBrain = /^\/brain/; botRegexReject = /^\/rejected/;
-      botRegexSch = /^\/sch/; botRegexGreat = /^\/greatness/; botRegexCh = /^\/cheese/;
+      botRegexSch = /^\/sch/; botRegexGreat = /^\/greatness/; botRegexGoat2 = /^\/goatest/; botRegexCardz = /^\/(.*cardz)(\s+.*)/;
+      botRegexTemp = /^\/nfl/; botRegexBF = /^\/battlefield/; 
       botRegexSiege = /^\/siege/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -321,13 +322,27 @@ function respond() {
     postMessage("http://i.imgur.com/1GKKJi0.jpg")
     this.res.end();
     }       
-      else if(request.text && botRegexCh.test(request.text)) {
+      else if(request.text && botRegexGoat2.test(request.text)) {
     this.res.writeHead(200);
-    postMessage(“/tony“)
+    postMessage("https://media0.giphy.com/media/pnMm35fFjw16U/giphy.gif")
     this.res.end();
-    }       
-
-   else if(request.text && botRegexSiege.test(request.text)) {
+    }         
+      else if(request.text && botRegexCardz.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://www.directordealabanza.com/wp-content/uploads/2014/03/no.jpg")
+    this.res.end();
+    }          
+    else if(request.text && botRegexTemp.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://docs.google.com/spreadsheets/d/1yrYajVQDuf-TjSDCUZ3l4fvhap3YHiXeMVsaFLUilOc/edit#gid=1029800088")
+    this.res.end();
+    }   
+     else if(request.text && botRegexBF.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://i3.kym-cdn.com/photos/images/original/000/210/690/battlefield_3_vs_pike.jpg")
+    this.res.end();
+    }  
+  else if(request.text && botRegexSiege.test(request.text)) {
     this.res.writeHead(200);
     if(0.6 >= Math.random() > 0.3)
       postMessage(siege1);
