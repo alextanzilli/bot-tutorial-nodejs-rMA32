8d9e3c3fcf90233735a289d2b8079d093bc2c845
alextanzilli <alextanzilli@gmail.com>
2017-12-05 22:07:56 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -104,7 +104,7 @@ function respond() {
   }
   else if(request.text && botRegextrades.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://docs.google.com/spreadsheets/d/1OUQwzq1jgVKfeOQkxmrEE7-uMjpcf-KFGErVeklJWPk/edit#gid=1408370000");
+    postMessage("https://docs.google.com/spreadsheets/d/1OUQwzq1jgVKfeOQkxmrEE7-uMjpcf-KFGErVeklJWPk/edit#gid=1379305614");
     this.res.end();
   }
   else if(request.text && botRegexFats.test(request.text)) {
@@ -124,7 +124,7 @@ function respond() {
   }
   else if(request.text && botRegexProp.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://goo.gl/forms/NirIQUcPgGPtWd7a2");
+    postMessage("https://goo.gl/forms/XlIbAOOYZPsqP8kv1");
     this.res.end();
   }
     else if(request.text && botRegexRec.test(request.text)) {
