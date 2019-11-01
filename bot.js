56a83ea0bf05668bc6612e357140be669532b011
alextanzilli <alextanzilli@gmail.com>
2019-10-31 17:35:15 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -104,7 +104,7 @@ function respond() {
   }
   else if(request.text && botRegextrades.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://docs.google.com/spreadsheets/d/1ozYieBbkLrs5ZWwECN_3wJEHPRYll8wrQ6L1cF6Mg6Y/edit?usp=sharing");
+    postMessage("https://docs.google.com/spreadsheets/d/1ozYieBbkLrs5ZWwECN_3wJEHPRYll8wrQ6L1cF6Mg6Y/edit#gid=362529474");
     this.res.end();
   }
   else if(request.text && botRegexFats.test(request.text)) {
